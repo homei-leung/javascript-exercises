2f2be33602de911a9799cddaa18c41e23cd09a3b
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-09 15:24:10 -0400

    add sumAll function with positive integer conditions and return sum, remove skip for tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(lowerLimit, upperLimit) {
+    let sum = 0;
+    let numSet = [];
 
+    if ((lowerLimit%1 === 0 && upperLimit%1 === 0) &&
+        (lowerLimit > 0 && upperLimit > 0) &&
+        (typeof lowerLimit == "number" && typeof upperLimit == "number")){
+
+        if (lowerLimit <= upperLimit){
+            for (let j = lowerLimit; j <= upperLimit; j++){
+                numSet.push(j);
+            }
+        }else {
+            for (let j = upperLimit; j <= lowerLimit; j++){
+                numSet.push(j);
+            }
+        }
+
+        for (let i = 0; i < numSet.length; i++){
+            sum += numSet[i];
+        }
+
+        return sum;
+    }else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
