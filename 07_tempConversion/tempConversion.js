e0155ec45592d8a33254e795740426bb932a70fd
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-09 16:15:18 -0400

    add conversion calculation and parseFloat to first decimal value, remove skip from tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let celsius = (degreesF - 32) * (5/9);
+  if (celsius%1 === 0){
+    return celsius;
+  }else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let fahrenheit = degreesC * (9/5) + 32;
+  if (fahrenheit%1 === 0){
+    return fahrenheit;
+  }else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
