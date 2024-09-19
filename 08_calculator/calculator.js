ff38ec0cd9f287956af5b43b4abdc0276bb0403c
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-19 13:53:48 -0400

    add calculator functions, remove skip tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => {
+    return total + currentItem;
+    }, 0);
+  };
+
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0){
+    return 1;
+  }else if (a < 0){
+    return "A factorial of a number less than 0 cannot be computed."
+  }else {
+    let array = [];
+    for (let i = 1; i <= a; i++){
+      array.push(i);
+    }
+    return array.reduce((total, currentItem) => {
+      return total * currentItem;
+    }, 1);
+  };
 };
 
 // Do not edit below this line
