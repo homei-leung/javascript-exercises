7b7366322b42602a5662c24098afb498a7983fd8
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-19 15:09:46 -0400

    add palindromes function, remove skip tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let stringArray = [... cleanString];
+    const reverseString = stringArray.reverse().join("");
+    return cleanString === reverseString ? true : false;
 };
 
 // Do not edit below this line
