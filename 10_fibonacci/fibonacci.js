27461cbb772ef38df8fe48150111781aa2e1107f
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-20 12:13:07 -0400

    add fibonacci sequence function, remove skip tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        //If n is less than 0, return "OOPS"
+        return "OOPS";
+    } else {
+        // Create fibonacci sequence, up to n
+        let sequence = [0, 1];
+        for (let i = 1; i <= n; i++){
+            let nextNumber = sequence[i] + sequence[i-1];
+            sequence.push(nextNumber);
+        }
+        // return n
+        return sequence[n];
+    }
 };
 
 // Do not edit below this line
