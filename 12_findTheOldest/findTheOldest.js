5e1a014d8a48e322d8269a4bdacb37985547cdf1
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-20 14:37:38 -0400

    add function to find the oldest in an array, remove skip tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    // Calculate the age of the person
+    let ages = array.map((person) => {
+        if (!person.yearOfDeath){
+            let date = new Date();
+            return date.getFullYear() - person.yearOfBirth;
+        } else{
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    })
 
+    // Compare the age of each person to find the index of oldest person
+    let oldestIndex = 0;
+    const oldest = Math.max(...ages);
+    for (let i = 0; i < array.length; i++){
+        if (ages[i] === oldest){
+            oldestIndex = i;
+        }
+    }
+
+    // Return name of oldest person
+    return array[oldestIndex];
 };
 
 // Do not edit below this line
