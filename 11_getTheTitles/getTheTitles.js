1fec11bce2b9c9efa7b60cf847817903245f4748
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-20 12:41:32 -0400

    add function to retrieve book titles from an array containing objects with title as the first key value pair

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    // Get each values of each object and store it in an array
+    // Get the first value of the array (title) and push it to the titles array
+    for (let i = 0; i < array.length; i++){
+        let book = array[i]
+        let bookValues = Object.values(book);
+        let bookTitle = bookValues[0];
+        titles.push(bookTitle);
+    }
+    // Return the list of titles
+    return titles;
 };
 
 // Do not edit below this line
